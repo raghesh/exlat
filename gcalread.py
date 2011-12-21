5dfe8ee036f67aafe125a9eed6bfe97f187ef42d
Raghesh <raghesh.a@gmail.com>
2011-12-21 22:57:34 +0530

    Added function getListOfDictionary
    This function will return a list of dictionaries. Each dictionary will
    have details about a single event entry.

--- gcalread.py
+++ gcalread.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
+def getListofDictionary(List, EventName):
+  EventCount = List.count(EventName)
+  EventsList = []
+  End = 0
+  for EventIndex in range(0, EventCount):
+    Dict = {}
+    Start = List.index(EventName, End)
+    End = List.index('SUMMARY', Start)
+    KeyIndex = Start + 2
+    for Entry in List[Start + 1:End:2]:
+      Dict[Entry] = List[KeyIndex]
+      KeyIndex += 2
+    EventsList.append(Dict)
+
+  return EventsList
+
 F = open("basic.ics")
 Content = F.read()
-ListOfLines = Content.split('\r\n')
-getDictionaryofEvent('Compiler Class')
+NewContent = Content.replace('\r\n', ':')
+List = NewContent.split(':')
+print getListofDictionary(List, 'Compiler Class')
