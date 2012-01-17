c0fb461c8c8172081d8c74c5bdac2d961013e45a
Raghesh <raghesh.a@gmail.com>
2012-01-17 18:02:23 +0530

    exchange.py: Introducing concept template file
    Having discussed with Ershad it is decided to design the export feature
    as below
    
    The user provides a preferred template file. He needs to give some
    predefined string like "%data_goes_here". We insert the required data
    by replacing this string. If the user has not provided any template file
    a default template is used.
    
    This particular commit just tests the default template string

--- exchange.py
+++ exchange.py
@@ -1,19 +1,39 @@
 class ExportToLatex:
-  """This class implements the interface required to export
-     different formats to Latex.
+  """This class implements the interface required to export different formats
+     to Latex. The input is a template file which is preferred by the user. The
+     required data is inserted into predefined positions in the template file.
   """
 
-  def __init__(self):
-    self.LatexFile = open("output.tex", "w");
+  def __init__(self, TemplateFile = ""):
+    if (TemplateFile == ""):
+      self.TemplateString = self.defaultTemplateString()
+    else:
+      F = open(TemplateFile, "r")
+      self.TemplateString = read(F)
+      close(F)
 
-  def createLatexFile(self):
-    """Function which creates the basic latex file.
+  def defaultTemplateString(self):
+    """Function which returns the default template string. This is used if the
+       user has not provided the template file.
     """
-
-    Out = """\\documentclass[12pt]{article}
+    DefaultTemplateString = """\\documentclass[12pt]{article}
              \\begin{document}
-             hello
+             %data_goes_here
              \\end{document}"""
-    F = self.LatexFile
-    F.write(Out)
-    F.close()
+
+    return DefaultTemplateString
+
+  def getTemplateString(self):
+    return self.TemplateString
+
+
+def testExportToLatex():
+  DefaultTemplateString = """\\documentclass[12pt]{article}
+             \\begin{document}
+             %data_goes_here
+             \\end{document}"""
+  Obj = ExportToLatex()
+  assert Obj.getTemplateString() == DefaultTemplateString
+
+if __name__ == '__main__':
+  testExportToLatex()
